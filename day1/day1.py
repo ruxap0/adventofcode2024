deaba6e679a0a13b2d0345cfeab524dba397f2a7
ruxap0 <florian.jost.foot@gmail.com>
2024-12-01 13:24:55 +0100

    Day 1 done !

---
+++ day1/day1.py
@@ -0,0 +1,20 @@
+list1 = []
+list2 = []
+answer = 0
+
+with open('textfile1.txt') as f:
+    for l in f.readlines():
+        n1, n2 = l.strip().split('   ')
+        list1.append(int(n1))
+        list2.append(int(n2))
+    
+    list1.sort()
+    list2.sort()
+
+    while list1:
+        answer += abs(list1[0] - list2[0])
+    
+        list1.pop(0)
+        list2.pop(0)
+
+print(answer)
---
+++ day1/day1_2.py
@@ -0,0 +1,16 @@
+list1 = []
+list2 = []
+answer = 0
+
+with open('textfile1.txt') as f:
+    for l in f.readlines():
+        n1, n2 = l.strip().split('   ')
+        list1.append(int(n1))
+        list2.append(int(n2))
+
+    for number in list1:
+        count = list2.count(number)
+        answer += count * number
+        
+
+print(answer)
